e16cc4834686c119a0f4421c6d2482cd63c916c7
faweil <fabianweiland@arcor.de>
2024-02-08 16:05:41 -0800

    updated random_Agent

--- randomAgent.py
+++ randomAgent.py
@@ -1,41 +1,56 @@
-def randomized_agent():
-    environment = initialize_environment(M, N, dirt_percentage)
+import numpy as np
+from environment import Environment
+class RandomAgent:
 
-    dirtCount = 0
-    positionRow = random.randint(1, N-1)
-    positionCol = random.randint(1, M-1)
+    def __init__(self, startX, startY):
+        self.positionCol = startX
+        self.positionRow = startY
 
-    if(environment[positionRow][positionCol] == 1):
-        environment[positionRow][positionCol] = 0
-        dirtCount += 1
+    def action(self, env):
+        #update agent environment
+        env.update_agent_path(self.positionCol, self.positionRow)
 
-    run = 0
-    while(run < 15):        #15 runs
-        randomAction = random.randrange(4)  # 0 -> up; 1 -> down; 2 -> right; 3 -> left
-        match randomAction:
-            case 0: positionRow += 1
-            case 1: positionRow -= 1
-            case 2: positionCol += 1
-            case 3: positionCol -= 1
+        act = self.randomAction()
+        match act:
+            case "up"   : self.positionRow -= 1
+            case "down" : self.positionRow += 1
+            case "right": self.positionCol += 1
+            case "left" : self.positionCol -= 1
 
-        if((positionRow < 0) or (positionRow >= N)):
-            return dirtCount
+        # stop vacuum if outside the grid
+        if((self.positionRow < 0) or (self.positionRow >= env.get_bounds()[1])):
+            return -1
         
-        if((positionCol < 0) or (positionCol >= N)):
-            return dirtCount
+        if((self.positionCol < 0) or (self.positionCol >= env.get_bounds()[0])):
+            return -1
 
-        if(environment[positionRow][positionCol] == 1):
-            environment[positionRow][positionCol] = 0
-            dirtCount += 1
+        #check if dirty and update the floor environment
+        if(env.is_dirty(self.positionCol, self.positionRow)):
+            env.update_env(self.positionCol, self.positionRow)
+
+    def randomAction(self):
+        randomAct = np.random.choice(["up", "down", "right", "left"])
+        return randomAct
         
-        run += 1
-    
-    return dirtCount
+
+    def visualize_agent_mevement(self, env):
+        pass
 
 
-def performance_Random_Agent():
-    result = 0
-    for _ in range(100):
-        result += randomized_agent()
-    print("Random_Agent:\nAverage performance of 100 runs is: %f" %(result / 100))
-performance_Random_Agent()
+#test
+env = Environment(10, 10)
+env.add_dirt(60)
+agent = RandomAgent(5, 5)
+env.visualize()
+print(env.get_stats())
+
+for i in range(100):
+    result = agent.action(env)
+    if(result == -1):
+        break
+
+env.visualize()
+print(env.get_stats())
+
+
+

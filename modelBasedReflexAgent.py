5d054da33139d227264f680559959d54665fd16a
sihambyte <sihamargaw@gmail.com>
2024-02-10 23:20:01 -0800

    added path and dirty_cells_cleaned attributes for plotting

--- modelBasedReflexAgent.py
+++ modelBasedReflexAgent.py
@@ -4,27 +4,27 @@ import random
 class ModelBasedReflexAgent:
     def __init__(self, start_x, start_y):
         self.location = (start_x, start_y)  
-        self.model = {
-            "PerceptHistory": {}  # Initializing percept history
-        }
+        self.history = {}
+        self.path = [(start_x, start_y)]  # Initialize path with starting location
+        self.dirty_cells_cleaned = [0]
     def sense(self, env):
         # Sensing the current location's state and update the agent's model
         x, y = self.location
         percept = "Dirty" if env.is_dirty(x, y) else "Clean"
-        self.model["PerceptHistory"][self.location] = percept
+        self.history[self.location] = percept
     def act(self, env):
         # Deciding the action based on the percept history
         C, R= env.get_bounds()
         x, y = self.location
-        if self.model["PerceptHistory"].get(self.location) == "Dirty":
+        if self.history.get(self.location) == "Dirty":
             return "Suck"
-        elif (x, y + 1) in self.model["PerceptHistory"] and self.model["PerceptHistory"][(x, y + 1)] == "Dirty":
+        elif (x, y + 1) in self.history and self.history[(x, y + 1)] == "Dirty":
             return "Down"
-        elif (x, y - 1) in self.model["PerceptHistory"] and self.model["PerceptHistory"][(x, y - 1)] == "Dirty":
+        elif (x, y - 1) in self.history and self.history[(x, y - 1)] == "Dirty":
             return "Up"
-        elif (x + 1, y) in self.model["PerceptHistory"] and self.model["PerceptHistory"][(x + 1, y)] == "Dirty":
+        elif (x + 1, y) in self.history and self.history[(x + 1, y)] == "Dirty":
             return "Right"
-        elif (x - 1, y) in self.model["PerceptHistory"] and self.model["PerceptHistory"][(x - 1, y)] == "Dirty":
+        elif (x - 1, y) in self.history and self.history[(x - 1, y)] == "Dirty":
             return "Left"
         else:
             # If no dirty percept nearby, moves randomly
@@ -40,6 +40,7 @@ class ModelBasedReflexAgent:
         x, y = self.location
         if action == "Suck":
             env.update_env(x, y)
+            self.dirty_cells_cleaned.append(self.dirty_cells_cleaned[-1] + 1)
         elif action == "Down" and y < R-1:
             self.location = (x, y+1)
             env.update_agent_path(x, y+1)
@@ -52,6 +53,10 @@ class ModelBasedReflexAgent:
         elif action == "Left" and x>0:
             self.location = (x-1, y)
             env.update_agent_path(x-1, y)
+
+
+        # Update path with the new location after any move or action
+        self.path.append(self.location)
 
 #test
 env = Environment(10, 10)
